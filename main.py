0f4436d02cace7f3824b4f439235b3fee1762dc7
anissemohamed <anisse.mohamed@gmail.com>
2014-07-20 20:42:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import smbus
 #Init I2C port 1
 myI2c = smbus.SMBus(1)    
 
-#Loop condition
-readAgain = True
-
 #Slave address
 AK8963_ADDR       = 0x0C
 
@@ -25,13 +22,14 @@ AK8963_REG_CTRL1  = 0x0A
 AK8963_REG_CTRL2  = 0x0B
 
 
-while readAgain == True :
+while 1 :
   
   #Init AK8963
   myI2c.write_byte_data(AK8963_ADDR, AK8963_REG_CTRL1, 0x16)
 
   #Read sensor value
   sampleBuffer = myI2c.read_i2c_block_data(AK8963_ADDR, AK8963_REG_HXL, 6)
+  myI2c.read_byte_data(AK8963_ADDR, AK8963_REG_ST2)
   
   #Print data
   print "magX =",sampleBuffer[0] * 255 + sampleBuffer[1]
@@ -40,6 +38,7 @@ while readAgain == True :
   
   #Delay
   time.sleep(1)
+  
 
 
   
