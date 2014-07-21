0f20209f299883898adf563948ae8d0bcf9106af
anissemohamed <anisse.mohamed@gmail.com>
2014-07-21 16:30:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from time import sleep
 from sensor_AK8963 import*
+from csv import*
 
+outputFile = open('./MAG_log.csv', 'wb')
+writer = writer(outputFile,  delimiter='\t', quotechar='"')
 
 while 1 :
   
@@ -10,11 +13,18 @@ while 1 :
   #Read sensor value
   sampleBuffer = AK8963_read()
   
+  #Concatenate data
+  magX = sampleBuffer[0] * 255 + sampleBuffer[1]
+  magY = sampleBuffer[2] * 255 + sampleBuffer[3]
+  magZ = sampleBuffer[4] * 255 + sampleBuffer[5]
+  
+  #Print data onto the CSV file
+  #TODO
+  
   #Print data
-  print "magX =",sampleBuffer[0] * 255 + sampleBuffer[1]
-  print "magY =",sampleBuffer[2] * 255 + sampleBuffer[3]
-  print "magZ =",sampleBuffer[4] * 255 + sampleBuffer[4]
-  print " "
+  print "magX =",magX
+  print "magY =",magY
+  print "magZ =",magZ
   
   #Delay
   time.sleep(1)
