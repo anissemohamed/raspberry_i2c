eafe85bcf8b36ade9b74117988892c8395c22bde
anissemohamed <anisse.mohamed@gmail.com>
2014-07-21 17:24:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from time import sleep
 from sensor_AK8963 import*
 from csv import*
 
+#Open CSV file
 outputFile = open('./MAG_log.csv', 'wb')
-writer = writer(outputFile,  delimiter=';', quotechar='"')
+outputFile.seek(0, 0)
 
-#Start AK8963
+#Write header
+writer = writer(outputFile,  delimiter=';', quotechar='"')
+writer.writerow(['magX', 'magY', 'magZ'])
+
+#Init nbLine
+lineNb = 1
+
+#Start sensor
 AK8963_start()
 
 while 1 :
@@ -19,7 +27,11 @@ while 1 :
   magZ = sampleBuffer[4] * 255 + sampleBuffer[5]
   
   #Print data onto the CSV file
-  outputFile.seek(0, 2)
+  if(lineNb == 32):
+    lineNb = 1
+  else:
+    lineNb += 1
+  outputFile.seek(lineNb, 0)
   writer.writerow([magX, magY, magZ])
   
   #Print data
